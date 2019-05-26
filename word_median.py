8848da8febf98678a4131568a112b7cfb981575d
Florent Ayot <florent.ayot33@gmail.com>
2019-05-26 19:55:05 +0800

    Addedd the writing method of the result

--- word_median.py
+++ word_median.py
@@ -4,6 +4,7 @@ filename = './wc_input/words.txt'
 file = open(filename, 'r')
 dic =dict()
 nb = []
+result = open('wc_result.txt', 'w+')
 
 for line in file:
     line = line.rstrip().rstrip('.').rstrip(',')
@@ -15,17 +16,8 @@ for line in file:
         print(nb)
         r = statistics.median(nb)
         print (r)
+    result.write("%s\r %s\r %s\r\n" % (line, a, r) )
 
 
-# for line in file:
-#     line = line.rstrip().rstrip('.').rstrip(',')
-#     wds = line.split()
-#     for w in wds:
-#         if w in dic:
-#             dic[w] = dic[w] + 1
-#         else:
-#             dic[w] = 1
 
-# result = open('wc_result.txt', 'w+')
-# for key in sorted(dic.keys(), key=str.lower):
-#     result.write("%s: %s\r\n" % (key, dic[key]))
+    # result.write("%s: %s\r\n" % (wds, r))
