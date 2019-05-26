fd11c2a899ee0005f998b5020cfc6de6b7edf0a0
Florent Ayot <florent.ayot33@gmail.com>
2019-05-26 19:31:09 +0800

    Calculating the median of each words in a line

--- word_median.py
+++ word_median.py
@@ -1,3 +1,31 @@
-filename = open('./wc_input/text.txt', 'r')
+import statistics
 
-dic = dict()
+filename = './wc_input/words.txt'
+file = open(filename, 'r')
+dic =dict()
+nb = []
+
+for line in file:
+    line = line.rstrip().rstrip('.').rstrip(',')
+    wds = line.split()
+    a = [len (wds)]
+    print (a)
+    for b in a:
+        nb.extend(a)
+        print(nb)
+        r = statistics.median(nb)
+        print (r)
+
+
+# for line in file:
+#     line = line.rstrip().rstrip('.').rstrip(',')
+#     wds = line.split()
+#     for w in wds:
+#         if w in dic:
+#             dic[w] = dic[w] + 1
+#         else:
+#             dic[w] = 1
+
+# result = open('wc_result.txt', 'w+')
+# for key in sorted(dic.keys(), key=str.lower):
+#     result.write("%s: %s\r\n" % (key, dic[key]))
